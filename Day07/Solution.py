f63c629d7d850beb6a567237cdfacc65546e1465
Kellthuzad <kellen.f.swain@gmail.com>
2021-12-14 10:33:20 -0700

    Update Solution.py

--- Day07/Solution.py
+++ Day07/Solution.py
@@ -14,33 +14,11 @@ def main():
     for num in tempNums:
         nums.append(int(num))
 
-    # ans = bSearch(nums, max(nums), min(nums), sentScore)
     # print(f'answer:{ans}')
     dumbWay(nums)
    
 
 sentScore = 999999999999999
-
-
-def bSearch(input, upper, lower, bestScore):
-    print(upper)
-    print(lower)
-    mid = (upper + lower)// 2
-    
-    score = 0
-    scores = []
-    for num in input:
-        score += abs(num - mid)
-
-    scores.append(score)
-    print(f'mid:{mid}')
-    print(f'score:{score}')
-    
-    if upper > (lower +1) and score < bestScore:
-         scores.append(bSearch(input, upper, mid, score))
-         scores.append(bSearch(input, mid, lower, score))
-
-    return min(scores)
 
 def dumbWay(input):
     bestScore = 99999999999999999999999
