6e09c9cfc4aa372e2cbc8f628f8afb283f96f22a
Kellthuzad <kellen.f.swain@gmail.com>
2021-12-15 06:40:19 +0000

    YAY IM NOT DUMB

--- Day15/Solution.py
+++ Day15/Solution.py
@@ -1,10 +1,60 @@
+import heapq
 
 def main():
-    filehandle = open("./Day14/input.txt")
+    filehandle = open("./Day15/input.txt")
     lines = []
 
     for line in filehandle:
         lines.append(line.strip())
-        print(line)
+    
+    grid = parse(lines)
+    print(dijkstras(grid))
+
+def dijkstras(grid):
+    visited = set()
+    paths = [(0, (0,0))]
+    while paths:
+        item = heapq.heappop(paths)
+        val = item[0]
+        y,x = item[1]
+        if (y,x) in visited:
+            continue
+        visited.add((y,x))
+        if x == (len(grid[0])*5) -1  and y == (len(grid)*5) -1:
+            return val
+        
+        for dy,dx in [(1,0),(-1,0),(0,1),(0,-1)]: 
+            if 0 <= x+dx < len(grid[0])*5 and 0 <= y+dy < len(grid)*5 and (y+dy, x+dx) not in visited:
+                modX = x+dx
+                modY = y+dy
+                gridVal = hereThereBeDragons(modX, modY, grid)
+
+                heapq.heappush(paths, (val+ gridVal, (y+dy,x+dx)))
+
+def parse(lines):
+    grid = []
+
+    for line in lines:
+        row = []
+        for char in line:
+            row.append(int(char))
+        grid.append(row)
+
+    return grid
+
+def hereThereBeDragons(modX, modY, grid):
+    modifier = 0
+    if modX >= len(grid[0]):
+        modifier += modX//len(grid[0])
+        modX = modX % len(grid[0])
+    if modY >= len(grid):
+        modifier += modY//len(grid)
+        modY = modY % len(grid)
+
+    gridVal = grid[modY][modX] + modifier
+    if gridVal > 9:
+        gridVal +=1
+        gridVal = gridVal % 10
+    return gridVal
 
 main()
